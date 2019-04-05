409657112cd0d53bb7ee39ce420e90468afd766f
linux08 <abimbola130@gmail.com>
2019-04-05 17:50:40 +0100

    Added feature to get location

--- android/app/src/main/java/com/selamobile/MainApplication.java
+++ android/app/src/main/java/com/selamobile/MainApplication.java
@@ -3,6 +3,7 @@ package com.selamobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.levelasquez.androidopensettings.AndroidOpenSettingsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AndroidOpenSettingsPackage(),
             new VectorIconsPackage(),
             new ImagePickerPackage(),
             new RNGestureHandlerPackage()
